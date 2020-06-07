7281a4a3e9d4b5fc7ce9aabebfe92e3d701dc397
Obagade <rotimiobags@gmail.com>
2020-06-07 18:59:22 +0200

    added the javascript and the styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,37 @@
-alert("conn")
+//check off todos
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+    // if($(this).css("color") === "rgb(128, 128, 128)"){
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     });
+    // } else {
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //     });
+    // }
+   
+});
+
+//to delete todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>")
+    }
+})
+
+$(".fa-plus-circle").click(function(){
+    $("input[type='text'").fadeToggle()
+})
